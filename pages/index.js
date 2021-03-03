c5ccf8dd3bd2cf3e8b11347b7648afd0f6d68d84
Curtis Sapp <curtmsapp@gmail.com>
2021-03-03 16:59:24 -0700

    Prints dog promise. Moved to component did mount

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,11 @@ import Dog9 from '../public/images/dog9.png';*/
 
 export default class Home extends React.Component{
 
-  render() {
+	componentDidMount() {
+		const dogs = fetch('http://localhost:3000/api/dogs').then((res) => console.log(res.json()));
+	}
+
+	render() {
 	/*const name = 'Josh Perez';
 	var element = <h1>Hello, {name}</h1>;*/
 	const added = "added text";
@@ -26,7 +30,7 @@ export default class Home extends React.Component{
 	
 	//const imageName = require({Dog1});
 
-	const dogs = fetch('localhost:3000/api/dogs').then((res) => console.log(res.json()));
+
 	
 	//const out = res[0];
 	
