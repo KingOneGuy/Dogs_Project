74c22230e0b4fbd2966de8c19c3ccb36b1ea572c
Frans <systemskog@gmail.com>
2021-03-09 04:01:24 -0700

    Remove comments

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,6 @@
 import styles from '../styles/Home.module.css';
 import React from 'react';
 import Image from 'react-dom';
-//import useSWR from 'swr';
-
-//import Dog1 from '../public/images/dog1.png';
-/*import Dog2 from '../public/images/dog2.png';
-import Dog3 from '../public/images/dog3.png';
-import Dog4 from '../public/images/dog4.png';
-import Dog5 from '../public/images/dog5.png';
-import Dog6 from '../public/images/dog6.png';
-import Dog7 from '../public/images/dog7.png';
-import Dog8 from '../public/images/dog8.png';
-import Dog9 from '../public/images/dog9.png';*/
-//import html from '../pages/test.html';
 
 export default class Home extends React.Component{
 
@@ -21,24 +9,12 @@ export default class Home extends React.Component{
 	}
 
 	render() {
-	/*const name = 'Josh Perez';
-	var element = <h1>Hello, {name}</h1>;*/
 	const added = "added text";
 	const JSX_Ex = <p> this is JSX value {added} </p>;
-	
-	//const fetcher = (url) => fetch(url).then((res) => res.json())
-	
-	//const imageName = require({Dog1});
-
-	//const dogs = fetch('https://localhost:3000/api/dogs').then((res) => console.log(res.json()));
-	
-	//const out = res[0];
 	
 
     return (
 		<img src="/images/dog1.png" alt="Dog1"/>
-		//<img src={imageName} />
-		//<p1>test</p1>
 	)
   }
 }
