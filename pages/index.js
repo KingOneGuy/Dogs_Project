c39def70907f45327552323267f57f6f0305d515
Frans <systemskog@gmail.com>
2021-03-09 04:37:06 -0700

    constructor(props)
    Also some formatting changes

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,21 @@ import Image from 'react-dom';
 
 export default class Home extends React.Component{
 
+	constructor(props) {
+		super(props);
+	}
+
 	componentDidMount() {
 		const dogs = fetch('http://localhost:3000/api/dogs').then((res) => console.log(res.json()));
 	}
 
 	render() {
-	const added = "added text";
-	const JSX_Ex = <p> this is JSX value {added} </p>;
+		const added = "added text";
+		const JSX_Ex = <p> this is JSX value {added} </p>;
 	
 
-    return (
-		<img src="/images/dog1.png" alt="Dog1"/>
-	)
+   		return (
+			<img src="/images/dog1.png" alt="Dog1"/>
+		)
   }
 }
