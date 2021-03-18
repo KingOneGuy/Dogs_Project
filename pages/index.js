df39f71020428e2f09d602f40f08fbf43212e3fa
Frans <systemskog@gmail.com>
2021-03-18 00:28:21 -0700

    Note about render
    Contains example explaining that value of fetched object may not return in time for render

--- pages/index.js
+++ pages/index.js
@@ -16,16 +16,13 @@ export default class Home extends React.Component{
 
    		return (
 			<html>
-				<Dogs src="/images/dog1.png"/>
+				<DogList />
 			</html>
 		)
   	}
 }
 
-class Dogs extends React.Component{
-	/*  Defined props:
-	 *  src
-	 */
+class DogList extends React.Component{
 	
 	constructor(props){
 		super(props);
@@ -36,22 +33,70 @@ class Dogs extends React.Component{
 
 	componentDidMount() {
 		fetch('http://localhost:3000/api/dogs')
-			.then((res) => res.json())
-			.then((res) => this.setState(
+			.then(res => res.json())
+			.then(res => this.setState(
 				{
 					dogs: res.dogs
 				}
-			))
-			.then((res) => console.log(this.state));
+			));
+			//.then(res => console.log(this.state));
 	}
 
 	render(){
-		const {src} = this.props; // same as: const src = this.props.src;
+		const dogs = this.state.dogs; // local variable dogs
+		//console.log(this.state.dogs.src);
+
+		// If this check is not here, program will try to read values that don't exist
+		// componentDidMount takes time, meaning that render will run first
+		if(dogs != null){
+			dogs.map(i => {
+				console.log(i.src);
+				console.log(i.name);
+				console.log(i.description);
+			})
+		}
+
+		/*return(
+			<div>
+				{() => dogs.map(i => {
+					<Dog src={i.src} name={i.name} desc={i.description} />
+				})}
+			</div>
+		)*/
 
 		return(
 			<div>
-				<img src={src} alt="Dog1"/>
-				<h1> I am from dogs. </h1>
+				<img src={this.state.src} />
+				<h1>{this.state.name}</h1>
+				<p>test</p>
+			</div>
+		)
+	}
+}
+
+
+
+class Dog extends React.Component{
+	/*  Defined props:
+	 *  src
+	 *  name
+	 *  desc
+	 */
+
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		const {src} = this.src; // same as: const src = this.props.src;
+		const {name} = this.name;
+		const {desc} = this.desc;
+
+		return(
+			<div>
+				<img src={src} />
+				<h1>{name}</h1>
+				<p>{desc}</p>
 			</div>
 		)
 	}
